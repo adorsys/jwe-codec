eac62349fe5feecdfac87c65961d08c8cb971247
radzom <github@radzom.net>
2018-09-21 16:01:04 +0200

    first working example for a jwe codec

---
+++ src/index.js
@@ -0,0 +1,44 @@
+const jose = require('node-jose')
+const hydra = require('hydration')
+
+function isKey(jwk) {
+    return (
+        [ 'oct' ].includes(jwk.kty) &&
+        [ 'A256GCMKW', 'A256GCM', undefined ].includes(jwk.alg) &&
+        [ 'enc', undefined ].includes(jwk.use) &&
+        jwk.k.match(/^[A-Za-z0-9_\-]{43}$/)
+    )
+}
+
+async function jwe(jwk) {
+    if (!isKey(jwk)) {
+        throw new Error(`Bad Key: ${JSON.stringify(jwk)}`)
+    }
+    const key = await jose.JWK.asKey(jwk)
+
+    async function encrypt(value) {
+        const dehydratedData = hydra.dehydrate({ value })
+        const cipher = await jose.JWE
+            .createEncrypt({ format: 'compact' }, key)
+            .update(JSON.stringify(dehydratedData))
+            .final()
+        return cipher
+    }
+    
+    async function decrypt(cipher) {
+        const { value } = await jose.JWE
+            .createDecrypt(key.keystore)
+            .decrypt(cipher)
+            .then(res => res.payload.toString())
+            .then(payload => hydra.hydrate(JSON.parse(payload)))
+        return value
+    }
+
+    return {
+        encrypt,
+        decrypt,
+    }
+}
+
+
+module.exports = jwe
