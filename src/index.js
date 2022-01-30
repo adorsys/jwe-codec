40f1ba1fee7fea4c247f56be303c8ff3aed1d825
Jan-Otto Kröpke <joe@adorsys.de>
2022-01-30 20:13:46 +0100

    feat: switch from node-jose to jose
    BREAKING CHANGE: How is it different from node-jose?
    node-jose is built to work in any javascript runtime, to be able to do that it packs a lot of polyfills and javascript implementation code in the form of node-forge, this significantly increases the footprint of the modules with dependencies that either aren't ever used or have native implementation available in the runtime already, those are often times faster and more reliable.

--- src/index.js
+++ src/index.js
@@ -1,35 +1,25 @@
-const jose = require('node-jose')
+const { importJWK, compactDecrypt, CompactEncrypt } = require('jose')
 const hydra = require('hydration')
 
-function isKey (jwk) {
-  return (
-    ['oct'].includes(jwk.kty) &&
-    ['A256KW', 'A256GCMKW', 'A256GCM', 'A128CBC-HS256', undefined].includes(jwk.alg) &&
-    ['enc', undefined].includes(jwk.use) &&
-    jwk.k.match(/^[A-Za-z0-9_-]{43}$/)
-  )
-}
+const encoder = new TextEncoder()
 
 async function jwe (jwk) {
-  if (!isKey(jwk)) {
-    throw new Error('bad key')
-  }
-  const key = await jose.JWK.asKey(jwk)
+  const secretKey = await importJWK(jwk, jwk.alg, true)
 
   async function encrypt (value) {
     const dehydratedData = hydra.dehydrate({ value })
-    const cipher = await jose.JWE.createEncrypt({ format: 'compact' }, key).final(
-      JSON.stringify(dehydratedData),
-      'utf8'
-    )
-    return cipher
+    const payload = JSON.stringify(dehydratedData)
+
+    return await new CompactEncrypt(encoder.encode(payload))
+      .setProtectedHeader({ alg: 'dir', enc: jwk.alg })
+      .encrypt(secretKey)
   }
 
   async function decrypt (cipher) {
-    const { value } = await jose.JWE.createDecrypt(key.keystore)
-      .decrypt(cipher)
-      .then(res => res.payload.toString())
-      .then(payload => hydra.hydrate(JSON.parse(payload)))
+    const { plaintext } = await compactDecrypt(cipher, secretKey)
+    const payload = new TextDecoder().decode(plaintext)
+
+    const { value } = hydra.hydrate(JSON.parse(payload))
     return value
   }
 
--- test/jwe.test.js
+++ test/jwe.test.js
@@ -11,53 +11,8 @@ const validJwks = [
   {
     kty: 'oct',
     use: 'enc',
-    alg: 'A256GCMKW',
-    k: '1234567890123456789012345678901234567890123'
-  },
-  {
-    kty: 'oct',
-    use: 'enc',
-    alg: 'A256KW',
-    k: '1234567890123456789012345678901234567890123'
-  },
-  {
-    kty: 'oct',
-    use: 'enc',
     alg: 'A128CBC-HS256',
     k: '1234567890123456789012345678901234567890123'
-  }
-]
-
-const corruptJwks = [
-  {
-    kty: 'EC',
-    use: 'enc',
-    alg: 'A256GCM',
-    k: '1234567890123456789012345678901234567890123'
-  },
-  {
-    kty: 'oct',
-    use: 'dec',
-    alg: 'A256GCM',
-    k: '1234567890123456789012345678901234567890123'
-  },
-  {
-    kty: 'oct',
-    use: 'enc',
-    alg: 'A128KW',
-    k: '1234567890123456789012345678901234567890123'
-  },
-  {
-    kty: 'oct',
-    use: 'enc',
-    alg: 'A256GCM',
-    k: '123456789012345678901234567890123456789012'
-  },
-  {
-    kty: 'oct',
-    use: 'enc',
-    alg: 'A256GCM',
-    k: '12345678901234567890123456789012345678901234'
   }
 ]
 
@@ -82,31 +37,19 @@ const testValues = [
   }
 ]
 
-corruptJwks.forEach((jwk, i) => {
-  test.cb(`checks jwk ${JSON.stringify(corruptJwks[i])}`, t => {
-    ;(async () => {
-      await t.throwsAsync(() => jwe(jwk), {message: 'bad key'}, 'key is bad')
-      t.end()
-    })()
-  })
-})
-
-validJwks.forEach((jwk, i) => {
+validJwks.forEach(jwk => {
   testValues.forEach((tv, i) => {
-    test.cb(`encrypt/decrypt with ${jwk.alg} ${JSON.stringify(testValues[i])}`, t => {
-      ;(async () => {
-        const codec = await jwe(jwk)
-        const cipher = await codec.encrypt(tv)
-        t.is(typeof cipher, 'string', 'encrypt gives string')
-        const parts = cipher.split('.')
-        t.is(parts.length, 5, 'with 5 parts')
-        parts.forEach(part => {
-          t.regex(part, /^[A-Za-z0-9_-]*$/, 'of Base64 encoded values')
-        })
-        const value = await codec.decrypt(cipher)
-        t.deepEqual(value, tv, 'decrypt gives correct value')
-        t.end()
-      })()
+    test(`encrypt/decrypt with ${jwk.alg} ${JSON.stringify(testValues[i])}`, async t => {
+      const codec = await jwe(jwk)
+      const cipher = await codec.encrypt(tv)
+      t.is(typeof cipher, 'string', 'encrypt gives string')
+      const parts = cipher.split('.')
+      t.is(parts.length, 5, 'with 5 parts')
+      parts.forEach(part => {
+        t.regex(part, /^[A-Za-z0-9_-]*$/, 'of Base64 encoded values')
+      })
+      const value = await codec.decrypt(cipher)
+      t.deepEqual(value, tv, 'decrypt gives correct value')
     })
   })
 })
